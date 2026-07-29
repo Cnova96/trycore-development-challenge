939f1ef6846b27f28ba44bb51371dc416d7c9920
Carlos Nova IT <cnova@infortributos.com>
2026-07-29 00:11:24 -0500

    feat: agregar importaciones de modulos al app.module

--- evm-calculation-service/src/app.module.ts
+++ evm-calculation-service/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProjectModule } from './modules/projects/projects.module';
+import { ActivityModule } from './modules/activities/activities.module';
+import { PrismaModule } from './config/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [ProjectModule, ActivityModule, PrismaModule],
 })
 export class AppModule {}
